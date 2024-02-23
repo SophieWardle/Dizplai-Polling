730dbab55726a7667cc332948cd2b0249213d445
SophieWardle <sophiewardle@ymail.com>
2024-02-23 19:39:03 +0000

    Frontend setup - Hello World! test
    - Added code to display index.html from running node app.js, can load in browser (>public directory)
    *Hello world! test :white_check_mark:
    
    - Moved assets folder provided into directory

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express()
+const path = require('path');
 const pollRoute = require('./routes/poll');
 const voteRoute = require('./routes/vote');
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/poll', pollRoute);
 app.use('/vote', voteRoute);
