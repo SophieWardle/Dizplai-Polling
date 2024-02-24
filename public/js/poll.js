e8475798ff1944573e995d0d448ace7efa318a37
SophieWardle <sophiewardle@ymail.com>
2024-02-24 17:58:38 +0000

    Created function to POST
    - Users can now post their vote, changes made to poll.js

--- public/js/poll.js
+++ public/js/poll.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     $.get('http://localhost:3000/poll', function(data) {
         const poll = data.polls[0];
+        const pollId = poll.poll_id;
         $('#polls-container').append(`<div>${poll.question}</div>`);
 
         poll.options.forEach(function(option) {
             const optionHtml = `
                 <div class="row justify-content-center">
-                    <button class="btn btn-primary option-btn">${option.option_text}</button>
+                    <button class="btn btn-primary option-btn" data-option-id="${option.option_id}">${option.option_text}</button>
                 </div>
             `;
             $('#options .row:first').append(optionHtml);
@@ -16,6 +17,22 @@ $(document).ready(function() {
         $('.option-btn').click(function() {
             $('.option-btn').removeClass('selected');
             $(this).addClass('selected');
+        });
+
+        //Submit button
+        $('#submit-btn').click(function(){
+            const selectedOptionId = $('.option-btn.selected').data('option-id');
+            if (selectedOptionId) {
+                $.post(`http://localhost:3000/vote/${pollId}/${selectedOptionId}`)
+                .done(function(response) {
+                    console.log('Vote submitted successfully');
+                })
+                .fail(function(error) {
+                    console.error('Failed to submit vote', error);
+                });
+            } else {
+                console.log('Press an option before submitting');
+            }
         });
     });
 });
