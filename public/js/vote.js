1b1cb65051273e9e2a4b16249025671f2a9c43e8
SophieWardle <sophiewardle@ymail.com>
2024-02-28 12:38:18 +0000

    Confirmation Page CSS
    - Created a percentage bar display to visually show the votes percentage
    
    - Centred the text and % within the container
    
    - Added layout with margin, positions and padding
    
    - Set Z-Indexes so that within the percentage/progress bar, the text and percentage would overlay the filled in div
    
    - CSS done mobile first, adjusted the styling for larger screens via media query

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,21 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const urlParams = new URLSearchParams(window.location.search);
     const pollId = urlParams.get('poll_id');
 
-    $.get(`http://localhost:3000/poll/${pollId}`, function(data) {
+    $.get(`http://localhost:3000/poll/${pollId}`, function (data) {
 
-    $('.results-container').empty();
+        $('.results-container').empty();
 
-    const totalVoteCount = data.totalVoteCount;
-    data.voteCounts.forEach(function(option) {
-        const percentage = Math.round((option.voteCount / totalVoteCount) * 100);
-        const optionHtml = `
-        <div class="option-box">
-            <div class="option-text">${option.option_text}</div>
-            <div class="vote-percentage">${percentage}%</div>
-        </div>
+        const totalVoteCount = data.totalVoteCount;
+        data.voteCounts.forEach(function (option) {
+            const percentage = Math.round((option.voteCount / totalVoteCount) * 100);
+            const optionHtml = `
+            <div class="option-box">
+    <div class="progress-box">
+        <div class="option-text">${option.option_text}</div>
+        <div class="percentage">${percentage}%</div>
+        <div class="filled" style="width: ${percentage}%"></div>
+    </div>
+</div>
         `;
-        $('.results-container').append(optionHtml);
+            $('.results-container').append(optionHtml);
+        });
     });
-   });
 });
